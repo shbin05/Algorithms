c8de9ba91e06cdd1a0b7b0e014b2dc84863c52a9
hb <shbin0519@gmail.com>
2025-03-23 17:43:22 +0900

    codetree solving

--- codetree/2024-1-1-1.py
+++ codetree/2024-1-1-1.py
@@ -1,162 +1,120 @@
-from collections import deque
 from copy import deepcopy
+from collections import deque
 
-K, M = map(int, input().split())
-board = []
-
-for _ in range(5):
-    line = list(map(int, input().split()))
-    board.append(line)
-
-wall = deque(map(int, input().split()))
+K,M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(5)]
 center = [
-    (1,1), (1,2), (1,3),
-    (2,1), (2,2), (2,3),
-    (3,1), (3,2), (3,3)      
+    (1,1),(2,1),(3,1),
+    (1,2),(2,2),(3,2),
+    (1,3),(2,3),(3,3)
 ]
-answer = []
+wall = deque(map(int,input().split()))
 
-def rotate(r,c,a):
-    a = a//90
-    for _ in range(a):
-        up = tmp[r-1][c-1:c+2]
-        left = [tmp[i][c-1] for i in range(r-1, r+2)]
-        right = [tmp[i][c+1] for i in range(r-1, r+2)]
-        down = tmp[r+1][c-1:c+2]
-        j = 0
-        for i in range(c-1, c+2):
-            tmp[r-1][i] = left[j]
-            j+=1
-        j = 0
-        for i in range(r-1, r+2):
-            tmp[i][c+1] = up[j]
-            j+=1
-        j = 0
-        for i in range(c+1,c-2,-1):
-            tmp[r+1][i] = right[j]
-            j+=1
-        j = 0
-        for i in range(r-1, r+2):
-            tmp[i][c-1] = down[j]
-            j+=1          
+def in_board(x,y):
+    if 0<=x<5 and 0<=y<5:
+        return True
+    else: return False
+
+def rotate(board,x,y):
+    c_board = deepcopy(board)
+    sx=x-1
+    sy=y-1
+
+    for i in range(3):
+        for j in range(3):
+            c_board[sx+i][sy+j] = board[sx+3-j-1][sy+i]
     
-    return
+    return c_board
 
-def find():
-    q = deque()
-    visited = deque()
+def find(board):
+    found = set()
+    for x in range(5):
+        for y in range(5):
+            q = deque()
+            q.append((x,y))
+            visited = deque()
+            tmp_found = []
+            
+            while q:
+                cx,cy = q.popleft()
+                for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+                    nx,ny = cx+dx,cy+dy
+                    if in_board(nx,ny) and board[cx][cy] == board[nx][ny] and (nx,ny) not in visited:
+                        q.append((nx,ny))
+                        visited.append((nx,ny))
+                        tmp_found.append((nx,ny))
 
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
+            if len(tmp_found) >= 3:
+                for tx,ty in tmp_found:
+                    found.add((tx,ty))
+    
+    return found
+
+
+for turn in range(K):
 
     cnt = 0
 
-    for i in range(5):
-        for j in range(5):
-            if tmp[i][j] != 0:
-                q.append((i,j))
-                visited.append((i,j))
-                m=1
-                while q:
-                    x,y = q.popleft() 
-                    for k in range(4):
-                        nx = x + dx[k]
-                        ny = y + dy[k]
-                        if 0 <= nx < 5 and 0 <= ny < 5 and (nx,ny) not in visited:
-                            if tmp[x][y] == tmp[nx][ny]:
-                                q.append((nx,ny))
-                                visited.append((nx,ny))
-                                m+=1
-                if m > 2:
-                    cnt+=len(visited)
-                    while visited:
-                        x,y = visited.popleft()
-                        tmp[x][y] = 0
-                else: visited.clear()
+    max_found = 0
+    tmp_board = list()
+    tmp_found = set()
+    tmp_angle=270
+
+    for x,y in center:
+        c_board = deepcopy(board)
+        b90 = rotate(c_board,x,y)
+        f90 = find(b90)
+        if len(f90) > max_found:
+            max_found = len(f90)
+            tmp_found = f90
+            tmp_board = b90
+            tmp_angle = 90
+        elif len(f90) == max_found and 90 < tmp_angle:
+            max_found = len(f90)
+            tmp_found = f90
+            tmp_board = b90
+            tmp_angle = 90
+
+        b180 = rotate(b90,x,y)
+        f180 = find(b180)
+        if len(f180) > max_found:
+            max_found = len(f180)
+            tmp_found = f180
+            tmp_board = b180
+            tmp_angle = 180
+        elif len(f180) == max_found and 180 < tmp_angle:
+            max_found = len(f180)
+            tmp_found = f180
+            tmp_board = b180
+            tmp_angle = 180
+        
+        b270 = rotate(b180,x,y)
+        f270 = find(b270)
+        if len(f270) > max_found:
+            max_found = len(f270)
+            tmp_found = f270
+            tmp_board = b270
+            tmp_angle = 270
     
-    return cnt          
+    if(max_found) == 0:
+        break
 
-def print_board(board):
-    for line in board:
-        print(line)
-    print()
+    board = tmp_board
+    found = tmp_found
+    found = sorted(found, key=lambda x: (x[1], -x[0]))
+    cnt += len(found)
 
-for I in range(K):
-    max_value = -1
-    angle = 0
-    coor = (0,0)
-    for r,c in center:
-        tmp = deepcopy(board)
-        rotate(r,c, 90)
-        score = find()
-        if score > max_value:
-            max_value = score
-            angle = 90
-            coor = (r,c)
-            rotated = tmp
-        elif score == max_value:
-            if 90 < angle: 
-                angle = 90
-                coor = (r,c)
-                rotated = tmp
-            elif angle == 90 and c < coor[1]:
-                coor = (r,c)
-                rotated = tmp
-            elif angle == 90 and  c == coor[1] and r < coor[0]:
-                coor = (r,c)
-                rotated = tmp
-
-        tmp = deepcopy(board)
-        rotate(r,c, 180)
-        score = find()
-        if score > max_value:
-            max_value = score
-            angle = 180
-            coor = (r,c)
-            rotated = tmp
-        elif score == max_value:
-            if 180 < angle: 
-                angle = 180
-                coor = (r,c)
-                rotated = tmp
-            elif angle == 180 and c < coor[1]:
-                coor = (r,c)
-                rotated = tmp
-            elif angle == 180 and  c == coor[1] and r < coor[0]:
-                coor = (r,c)
-                rotated = tmp
-
-        tmp = deepcopy(board)
-        rotate(r,c, 270)
-        score = find()
-        if score > max_value:
-            max_value = score
-            angle = 270
-            coor = (r,c)
-            rotated = tmp
-        elif score == max_value:
-            if angle == 270 and c < coor[1]:
-                coor = (r,c)
-                rotated = tmp
-            elif angle == 270 and  c == coor[1] and r < coor[0]:
-                coor = (r,c)
-                rotated = tmp
+    for x,y in found:
+        board[x][y] = wall.popleft()
     
-    score = max_value
-    if score == 0: break
-    tmp = rotated
-
     while True:
-        for p in range(5):
-            for q in range(4, -1, -1):
-                if tmp[q][p] == 0:
-                    n = wall.popleft()
-                    tmp[q][p] = n
-        result = find()
-        if result == 0 : break
-        score += result
+        found = find(board)
+        if len(found) == 0: break
         
-    board = tmp
+        found = sorted(found, key=lambda x: (x[1], -x[0]))
+        cnt+=len(found)
+        for x,y in found:
+            board[x][y] = wall.popleft()
+    
+    print(cnt, end=' ')
 
-    print(score, end=' ')
-        
